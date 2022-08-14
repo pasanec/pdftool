15b05137d671bca47ec4f5fb22fa5a771dbd8277
Immanuel Pasanec <immanuel@pasanec.de>
2022-08-14 18:00:02 +0200

    Feat: Added app and user information to logs.

--- lib/Service/LogService.php
+++ lib/Service/LogService.php
@@ -1,22 +1,45 @@
 <?php
+/**
+ *
+ * @copyright Copyright (c) 2022, Immanuel Pasanec (immanuel@pasanec.de)
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
 namespace OCA\PdfTool\Service;
 
 use Psr\Log\LoggerInterface;
 use Exception;
 
 class LogService {
+    private string $userId;
     private LoggerInterface $logger;
     private string $appName;
 
-    public function __construct(LoggerInterface $logger, string $appName){
-        $this->logger = $logger;
+    public function __construct(string $appName, LoggerInterface $logger, $userId){
+        $this->userId = $userId;
         $this->appName = $appName;
+        $this->logger = $logger;
     }
 
     public function log(string $message, Exception $e = NULL) : void {
         if ($e === NULL) {
             $this->logger->error($message);
-            return
+            return;
         }
         $extraContent = [
             'type' => gettype($e),
@@ -24,7 +47,9 @@ class LogService {
             'code' => $e->getCode(),
             'file' => $e->getFile(),
             'line' => $e->getLine(),
-            'trace' => $e->getTraceAsString();,
+            'trace' => $e->getTraceAsString(),
+            'user' => $this->userId,
+            'app' => $this->appName,
         ];
         $this->logger->error($message, $extraContent);
     }
