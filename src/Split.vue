9d033ae39137ad789424d9ab4af71c76d25f8b1b
Immanuel Pasanec <immanuel@pasanec.de>
2024-10-09 18:30:50 +0200

    Changes for current nextcloud version.

--- src/Split.vue
+++ src/Split.vue
@@ -1,20 +1,20 @@
 <!--
  @copyright Copyright (c) 2023 Immanuel Pasanec <i@pasanec.de>
-
+ 
  @author Immanuel Pasanec <i@pasanec.de>
-
+ 
  @license GNU AGPL version 3 or any later version
-
+ 
  This program is free software: you can redistribute it and/or modify
  it under the terms of the GNU Affero General Public License as
  published by the Free Software Foundation, either version 3 of the
  License, or (at your option) any later version.
-
+ 
  This program is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU Affero General Public License for more details.
-
+ 
  You should have received a copy of the GNU Affero General Public License
  along with this program. If not, see <http://www.gnu.org/licenses/>.
 -->
@@ -87,7 +87,6 @@ import '@nextcloud/dialogs/styles/toast.scss'
 import { generateUrl } from '@nextcloud/router'
 import { showError, showSuccess } from '@nextcloud/dialogs'
 import axios from '@nextcloud/axios'
-import { emit } from '@nextcloud/event-bus'
 
 
 export default {
@@ -140,10 +139,8 @@ export default {
 			console.info(data)
 			try {
 				const response = await axios.post(generateUrl('/apps/pdftool/merge'), data)
-				// TODO: Find a way to implement scrolling to the new file.
-				// await this.filelistObj.reload()
-				emit('files:node:deleted', node)
-				// this.filelistObj.scrollTo(response.data.substring(response.data.indexOf('/') + 1))
+				await this.filelistObj.reload()
+				this.filelistObj.scrollTo(response.data.substring(response.data.indexOf('/') + 1))
 			} catch (e) {
 				console.error(e)
 				showError(t('pdftool', 'Could not merge PDF.'))
--- src/mergeAction.ts
+++ src/mergeAction.ts
@@ -20,7 +20,16 @@
  *
  */
 import { generateFilePath } from '@nextcloud/router'
+import { Permission, Node, View, FileAction } from '@nextcloud/files'
+import { showInfo } from '@nextcloud/dialogs'
+import { translate as t } from '@nextcloud/l10n'
+import PQueue from 'p-queue'
+
+import CloseSvg from '@mdi/svg/svg/close.svg?raw'
+import NetworkOffSvg from '@mdi/svg/svg/network-off.svg?raw'
+import TrashCanSvg from '@mdi/svg/svg/trash-can.svg?raw'
 import { addNewFileMenuEntry, registerDavProperty, registerFileAction } from '@nextcloud/files'
+import { askConfirmation, canDisconnectOnly, canUnshareOnly, deleteNode, displayName, isTrashbinEnabled } from './pdfHelper.ts'
 
 import Vue from 'vue'
 import Merge from './Merge'
@@ -31,6 +40,104 @@ import Merge from './Merge'
 //  const merger = document.createElement('div')
 //  merger.setAttribute('id', 'pdftools-content')
 (function() {
+	export const action = new FileAction(
+	id: 'pdfmerge',
+	displayName,
+	iconSvgInline: (nodes: Node[]) => {
+		if (canUnshareOnly(nodes)) {
+			return CloseSvg
+		}
+
+		if (canDisconnectOnly(nodes)) {
+			return NetworkOffSvg
+		}
+
+		return TrashCanSvg
+	},
+
+	enabled(nodes: Node[]) {
+		return nodes.length > 1 && nodes
+			.map(node => node.permissions)
+			.every(permission => (permission & Permission.DELETE) !== 0)
+	},
+
+	async exec(node: Node, view: View) {
+		try {
+			console.info('PdfTool Multiselect action')
+			console.info(selection)
+				Vue.mixin({ methods: { t, n } })
+
+				new Vue({
+					el: '#pdftool-content',
+					render: h => h(Merge, {
+						props: {
+							// files: selection,
+							files: node,
+							// TODO: look for replacement.
+							filelistObj: fileList,
+						},
+					}),
+				})
+			let confirm = true
+
+			// If trashbin is disabled, we need to ask for confirmation
+			if (!isTrashbinEnabled()) {
+				confirm = await askConfirmation([node], view)
+			}
+
+			// If the user cancels the deletion, we don't want to do anything
+			if (confirm === false) {
+				showInfo(t('files', 'Deletion cancelled'))
+				return null
+			}
+
+			await deleteNode(node)
+
+			return true
+		} catch (error) {
+			logger.error('Error while deleting a file', { error, source: node.source, node })
+			return false
+		}
+	},
+
+	async execBatch(nodes: Node[], view: View): Promise<(boolean | null)[]> {
+		let confirm = true
+
+		// If trashbin is disabled, we need to ask for confirmation
+		if (!isTrashbinEnabled()) {
+			confirm = await askConfirmation(nodes, view)
+		} else if (nodes.length >= 5 && !canUnshareOnly(nodes) && !canDisconnectOnly(nodes)) {
+			confirm = await askConfirmation(nodes, view)
+		}
+
+		// If the user cancels the deletion, we don't want to do anything
+		if (confirm === false) {
+			showInfo(t('files', 'Deletion cancelled'))
+			return Promise.all(nodes.map(() => null))
+		}
+
+		// Map each node to a promise that resolves with the result of exec(node)
+		const promises = nodes.map(node => {
+			// Create a promise that resolves with the result of exec(node)
+			const promise = new Promise<boolean>(resolve => {
+				queue.add(async () => {
+					try {
+						await deleteNode(node)
+						resolve(true)
+					} catch (error) {
+						logger.error('Error while deleting a file', { error, source: node.source, node })
+						resolve(false)
+					}
+				})
+			})
+			return promise
+		})
+
+		return Promise.all(promises)
+	},
+
+	order: 100,
+	})
 	const PdftoolMultiselect = {
 		attach(fileList) {
 			fileList.registerMultiSelectFileAction({
