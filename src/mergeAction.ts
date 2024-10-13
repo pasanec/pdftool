67e8c7072d1993ea9833162009069064ffe4efcd
Immanuel Pasanec <immanuel@pasanec.de>
2024-10-13 18:42:08 +0200

    Fix: Missing curly bracket for object.

--- src/mergeAction.ts
+++ src/mergeAction.ts
@@ -23,7 +23,7 @@ import { generateFilePath } from '@nextcloud/router'
 import { Permission, Node, View, FileAction } from '@nextcloud/files'
 import { showInfo } from '@nextcloud/dialogs'
 import { translate as t } from '@nextcloud/l10n'
-import PQueue from 'p-queue'
+// import PQueue from 'p-queue'
 
 import CloseSvg from '@mdi/svg/svg/close.svg?raw'
 import NetworkOffSvg from '@mdi/svg/svg/network-off.svg?raw'
@@ -39,21 +39,20 @@ import Merge from './Merge'
 
 //  const merger = document.createElement('div')
 //  merger.setAttribute('id', 'pdftools-content')
-(function() {
-	export const action = new FileAction(
+	export const action = new FileAction({
 	id: 'pdfmerge',
 	displayName,
-	iconSvgInline: (nodes: Node[]) => {
-		if (canUnshareOnly(nodes)) {
-			return CloseSvg
-		}
-
-		if (canDisconnectOnly(nodes)) {
-			return NetworkOffSvg
-		}
-
-		return TrashCanSvg
-	},
+	// iconSvgInline: (nodes: Node[]) => {
+	// 	if (canUnshareOnly(nodes)) {
+	// 		return CloseSvg
+	// 	}
+	//
+	// 	if (canDisconnectOnly(nodes)) {
+	// 		return NetworkOffSvg
+	// 	}
+	//
+	// 	return TrashCanSvg
+	// },
 
 	enabled(nodes: Node[]) {
 		return nodes.length > 1 && nodes
@@ -138,101 +137,100 @@ import Merge from './Merge'
 
 	order: 100,
 	})
-	const PdftoolMultiselect = {
-		attach(fileList) {
-			fileList.registerMultiSelectFileAction({
-				name: 'pdftool',
-				displayName: t('pdftool', 'Pdf Merger'),
-				permissions: OC.PERMISSION_READ, // Don't know if it's working
-				iconClass: 'icon-pdf',
-				order: 0,
-				action: (selection) => {
-					console.info('PdfTool Multiselect action')
-					console.info(selection)
-					Vue.mixin({ methods: { t, n } })
-
-					new Vue({
-						el: '#pdftool-content',
-						render: h => h(Merge, {
-							props: {
-								files: selection,
-								filelistObj: fileList,
-							},
-						}),
-					})
-				},
-			})
-
-			fileList.$el.on('fileActionsReady', data => {
-				console.info('fileActionsReady')
-				console.info(fileList)
-				// fileList.fileMultiSelectMenu.toggleItemVisibility('pdftool', true)
-				// console.info('fileActionsReady')
-			})
-			fileList.$el.on('afterChangeDirectory', data => {
-				console.info('afterChangeDirectory')
-				fileList.fileMultiSelectMenu.toggleItemVisibility('pdftool', true)
-				console.info(fileList)
-				// console.info(data)
-			})
-			fileList.$el.on('change', data => {
-				console.info('PdfTool Multiselect change')
-				console.info(fileList.getSelectedFiles())
-				let showMultiselectAction = false
-				if (fileList.getSelectedFiles().length > 1) {
-					showMultiselectAction = true
-					fileList.getSelectedFiles().forEach(selection => {
-						console.info('selection.mimetype', selection.mimetype)
-						if (selection.mimetype !== 'application/pdf') {
-							console.info('selection.mimetype if TRUE', selection.mimetype)
-							showMultiselectAction = false
-						}
-					});
-				}
-				fileList.fileMultiSelectMenu.toggleItemVisibility('pdftool', showMultiselectAction)
-			})
-		},
-	}
-
-	OC.Plugins.register('OCA.Files.FileList', PdftoolMultiselect)
-
-	// const PdfToolSplitAction = {
-	//     attach: function (menu) {
-	//         menu.addMenuEntry({
-	//             id: 'file',
-	//             displayName: 'PdfTool',
-	//             templateName: 'MyNewTemplate',
-	//             iconClass: 'icon-pdf',
-	//             fileType: 'file',
-	//             actionHandler: function () {
-	//                 console.log('PdfTool Menu clicked.');
-	//             }
-	//         });
-	//     }
-	// }
-	// OC.Plugins.register('OCA.Files.NewFileMenu', PdfToolSplitAction);
-	OCA.Files.fileActions.registerAction({
-		mime: 'application/pdf',
-		name: 'pdfToolSplitter',
-		permissions: OC.PERMISSION_READ,
-		iconClass: 'icon-pdf',
-		// icon: OC.imagePath('gallery', 'gallery-dark'),
-		actionHandler: (fileName, context) => {
-			console.info('Split Action clicked')
-			console.info(fileName)
-			console.info(context.fileInfoModel.id)
-		},
-		displayName: t('pdftool', 'PDF Splitter')
-	})
-
-
-})()
-
-
-
-//  Vue.mixin({ methods: { t, n } })
-
-//  export default new Vue({
-//      el: '#pdftools-content',
-//      render: h => h(Merge),
-//  })
+// 	const PdftoolMultiselect = {
+// 		attach(fileList) {
+// 			fileList.registerMultiSelectFileAction({
+// 				name: 'pdftool',
+// 				displayName: t('pdftool', 'Pdf Merger'),
+// 				permissions: OC.PERMISSION_READ, // Don't know if it's working
+// 				iconClass: 'icon-pdf',
+// 				order: 0,
+// 				action: (selection) => {
+// 					console.info('PdfTool Multiselect action')
+// 					console.info(selection)
+// 					Vue.mixin({ methods: { t, n } })
+//
+// 					new Vue({
+// 						el: '#pdftool-content',
+// 						render: h => h(Merge, {
+// 							props: {
+// 								files: selection,
+// 								filelistObj: fileList,
+// 							},
+// 						}),
+// 					})
+// 				},
+// 			})
+//
+// 			fileList.$el.on('fileActionsReady', data => {
+// 				console.info('fileActionsReady')
+// 				console.info(fileList)
+// 				// fileList.fileMultiSelectMenu.toggleItemVisibility('pdftool', true)
+// 				// console.info('fileActionsReady')
+// 			})
+// 			fileList.$el.on('afterChangeDirectory', data => {
+// 				console.info('afterChangeDirectory')
+// 				fileList.fileMultiSelectMenu.toggleItemVisibility('pdftool', true)
+// 				console.info(fileList)
+// 				// console.info(data)
+// 			})
+// 			fileList.$el.on('change', data => {
+// 				console.info('PdfTool Multiselect change')
+// 				console.info(fileList.getSelectedFiles())
+// 				let showMultiselectAction = false
+// 				if (fileList.getSelectedFiles().length > 1) {
+// 					showMultiselectAction = true
+// 					fileList.getSelectedFiles().forEach(selection => {
+// 						console.info('selection.mimetype', selection.mimetype)
+// 						if (selection.mimetype !== 'application/pdf') {
+// 							console.info('selection.mimetype if TRUE', selection.mimetype)
+// 							showMultiselectAction = false
+// 						}
+// 					});
+// 				}
+// 				fileList.fileMultiSelectMenu.toggleItemVisibility('pdftool', showMultiselectAction)
+// 			})
+// 		},
+// 	}
+//
+// 	OC.Plugins.register('OCA.Files.FileList', PdftoolMultiselect)
+//
+// 	// const PdfToolSplitAction = {
+// 	//     attach: function (menu) {
+// 	//         menu.addMenuEntry({
+// 	//             id: 'file',
+// 	//             displayName: 'PdfTool',
+// 	//             templateName: 'MyNewTemplate',
+// 	//             iconClass: 'icon-pdf',
+// 	//             fileType: 'file',
+// 	//             actionHandler: function () {
+// 	//                 console.log('PdfTool Menu clicked.');
+// 	//             }
+// 	//         });
+// 	//     }
+// 	// }
+// 	// OC.Plugins.register('OCA.Files.NewFileMenu', PdfToolSplitAction);
+// 	OCA.Files.fileActions.registerAction({
+// 		mime: 'application/pdf',
+// 		name: 'pdfToolSplitter',
+// 		permissions: OC.PERMISSION_READ,
+// 		iconClass: 'icon-pdf',
+// 		// icon: OC.imagePath('gallery', 'gallery-dark'),
+// 		actionHandler: (fileName, context) => {
+// 			console.info('Split Action clicked')
+// 			console.info(fileName)
+// 			console.info(context.fileInfoModel.id)
+// 		},
+// 		displayName: t('pdftool', 'PDF Splitter')
+// 	})
+//
+//
+//
+//
+//
+// //  Vue.mixin({ methods: { t, n } })
+//
+// //  export default new Vue({
+// //      el: '#pdftools-content',
+// //      render: h => h(Merge),
+// //  })
