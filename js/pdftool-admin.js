a775379bda444ede111535d5147f8c5931dbbe72
Immanuel Pasanec <immanuel@pasanec.de>
2025-09-30 02:28:14 +0200

    Minified JS for: Fix: Settings not shown in server section.

---
+++ js/pdftool-admin.js
@@ -0,0 +1,26 @@
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/admin.js ***!
+  \**********************/
+(function (OC, OCA) {
+  'use strict';
+
+  $(document).ready(function () {
+    var adminUrl = OC.generateUrl('/apps/pdftool/settings/admin');
+    $('#pdftool_engine_setting input[type="radio"]').change(function () {
+      var engine = $(this).val();
+      OC.AppConfig.setValue('pdftool', 'pdf_tool_engine', engine);
+    });
+    $('#pdftool_max_pages').change(function () {
+      var maxPages = $(this).val();
+      OC.AppConfig.setValue('pdftool', 'pdf_tool_max_pages', maxPages);
+    });
+    $('#pdftool_max_pdfs').change(function () {
+      var maxPdfs = $(this).val();
+      OC.AppConfig.setValue('pdftool', 'pdf_tool_max_pdfs', maxPdfs);
+    });
+  });
+})(OC, OCA);
+/******/ })()
+;
+//# sourceMappingURL=pdftool-admin.js.map?v=bb5f94e2d2363038e25a
