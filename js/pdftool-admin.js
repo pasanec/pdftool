ac38a6c2caf9e8402970a2129f70819236e08283
Immanuel Pasanec <immanuel@pasanec.de>
2026-05-16 13:32:24 +0200

    Minified JS for: Fix: Admin settings not updating anymore.

--- js/pdftool-admin.js
+++ js/pdftool-admin.js
@@ -2,25 +2,41 @@
 /*!**********************!*\
   !*** ./src/admin.js ***!
   \**********************/
-(function (OC, OCA) {
+(function () {
   'use strict';
 
   $(document).ready(function () {
-    var adminUrl = OC.generateUrl('/apps/pdftool/settings/admin');
-    $('#pdftool_engine_setting input[type="radio"]').change(function () {
-      var engine = $(this).val();
-      OC.AppConfig.setValue('pdftool', 'pdf_tool_engine', engine);
-    });
-    $('#pdftool_max_pages').change(function () {
-      var maxPages = $(this).val();
-      OC.AppConfig.setValue('pdftool', 'pdf_tool_max_pages', maxPages);
-    });
-    $('#pdftool_max_pdfs').change(function () {
-      var maxPdfs = $(this).val();
-      OC.AppConfig.setValue('pdftool', 'pdf_tool_max_pdfs', maxPdfs);
-    });
+    const adminUrl = OC.generateUrl('/apps/pdftool/settings/admin');
+    const $engineInputs = $('#pdftool_engine_setting input[type="radio"]');
+    const $maxPagesInput = $('#pdftool_max_pages');
+    const $maxPdfsInput = $('#pdftool_max_pdfs');
+    const saveSettings = async function () {
+      $.ajax({
+        url: adminUrl,
+        method: 'POST',
+        contentType: 'application/json',
+        headers: {
+          requesttoken: OC.requestToken
+        },
+        data: JSON.stringify({
+          engine: $engineInputs.filter(':checked').val(),
+          maxPages: Number($maxPagesInput.val()),
+          maxPdfs: Number($maxPdfsInput.val())
+        }),
+        success: function () {
+          OC.Notification.showTemporary(OC.L10N.translate('pdftool', 'PDF Tool settings saved.'));
+        },
+        error: function (response) {
+          const message = response.responseJSON?.message || OC.L10N.translate('pdftool', 'Could not save PDF Tool settings.');
+          OC.Notification.showTemporary(message);
+        }
+      });
+    };
+    $engineInputs.change(saveSettings);
+    $maxPagesInput.change(saveSettings);
+    $maxPdfsInput.change(saveSettings);
   });
-})(OC, OCA);
+})();
 /******/ })()
 ;
-//# sourceMappingURL=pdftool-admin.js.map?v=bb5f94e2d2363038e25a
+//# sourceMappingURL=pdftool-admin.js.map?v=6df07bd374a02201373a
